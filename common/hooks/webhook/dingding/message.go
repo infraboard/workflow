a20261927a9f8f433f85af61e5dac87f8ce0d778
yumaojun03 <18108053819@163.com>
2021-07-27 23:18:17 +0800

    补充dingding和企业微信地址和文档

--- common/hooks/webhook/dingding/message.go
+++ common/hooks/webhook/dingding/message.go
@@ -1,1 +1,7 @@
 package dingding
+
+const (
+	URL_PREFIX = "https://oapi.dingtalk.com/robot/send"
+)
+
+// 自定义机器人接入: https://developers.dingtalk.com/document/app/custom-robot-access
---
+++ common/hooks/webhook/wechat/message.go
@@ -0,0 +1,7 @@
+package wechat
+
+const (
+	URL_PREFIX = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send"
+)
+
+// 群机器人配置说明: https://work.weixin.qq.com/api/doc/90000/90136/91770
